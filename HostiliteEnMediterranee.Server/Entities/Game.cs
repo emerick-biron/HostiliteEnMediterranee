ac0d794fdd795958c4cb945d5a2f71ccdb79cd6c
emerick-biron <emerick.biron@gmail.com>
2024-10-15 09:21:10 +0200

    update

--- HostiliteEnMediterranee.Server/Entities/Game.cs
+++ HostiliteEnMediterranee.Server/Entities/Game.cs
@@ -2,12 +2,19 @@ namespace HostiliteEnMediterranee.Server.Entities;
 
 public class Game
 {
+    public Game(Player player1, Player player2)
+    {
+        Players.Add(player1);
+        Players.Add(player2);
+    }
+
     public Guid Id { get; } = Guid.NewGuid();
     public List<Player> Players { get; } = [];
     private int CurrentPlayerIndex { get; set; } = 0;
     public GameStatus Status { get; private set; } = GameStatus.NotStarted;
 
-    public Game()
+    public void Start()
     {
+        Status = GameStatus.InProgress;
     }
 }
