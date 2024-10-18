bcd5bf1cb673a7c5af581deb45f795ceb5e98ed2
Louis Rocchi <louis.rocchi@veolia.com>
2024-10-18 15:27:26 +0200

    errerur

--- HostiliteEnMediterranee.Client/Entities/Ship.cs
+++ HostiliteEnMediterranee.Client/Entities/Ship.cs
@@ -22,6 +22,23 @@ namespace HostiliteEnMediterranee.Client.Entities
             SortCoordinates(Orientation);
         }
 
+        public Ship(char model, int size)
+        {
+            Model = model;
+            Size = size;
+            HitCoordinates = new List<CoordinatesDto>();
+            IsSinked = false;
+        }
+
+        public void UpdtateOpponentCoordinates(List<CoordinatesDto> coords)
+        {
+            HitCoordinates.AddRange(coords);
+            Coordinates.AddRange(coords);
+            IsSinked = true;
+            Orientation = GetOrientation();
+            SortCoordinates(Orientation);
+        }
+
         private string GetOrientation()
         {
             if (Coordinates[0].Row == Coordinates[1].Row)  return "horizontal";
--- HostiliteEnMediterranee.Models/Dto/AILevelDto.cs
+++ HostiliteEnMediterranee.Models/Dto/AILevelDto.cs
@@ -1,6 +1,4 @@
 namespace HostiliteEnMediterranee.Models.Dto;
-
-// ReSharper disable once InconsistentNaming
 public enum AILevelDto
 {
     Dumb,
